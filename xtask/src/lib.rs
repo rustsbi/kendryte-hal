54eabdd5ef8afae934487ce6d8edb684b85d032b
Longbing Zheng <97814641+ZhengLongBing@users.noreply.github.com>
2025-05-25 10:24:16 +0800

    Merge pull request #6 from ZhengLongBing/xtask
    refactor(xtask): correct gen-firmware to gen-image

--- xtask/src/lib.rs
+++ xtask/src/lib.rs
@@ -3,7 +3,12 @@
 //! This crate provides tools and utilities for working with the Kendryte K230 platform,
 //! including firmware generation, encryption, and image formatting.
 
+extern crate core;
+
+use crate::generate::image::EncryptionType;
 use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
 pub mod error;
 pub mod generate;
 
@@ -18,28 +23,28 @@ pub struct Cli {
 /// Subcommands for the xtask utility.
 #[derive(Subcommand, Debug)]
 pub enum Command {
-    /// Generate firmware for Kendryte K230.
+    /// Generate image for Kendryte K230.
     ///
     /// Ref: https://github.com/kendryte/canmv_k230/blob/main/tools/firmware_gen.py
-    GenFirmware {
+    GenImage {
         /// Input file path.
-        #[arg(long = "ifile", short = 'i')]
-        input: String,
+        #[arg(long = "input", short = 'i')]
+        input: PathBuf,
         /// Output file path (optional).
         ///
         /// Using default output path
         ///
-        ///     cargo xtask  gen-firmware -i target/riscv64gc-unknown-none-elf/release/uart-demo.bin
+        ///     cargo xtask  gen-image -i target/riscv64gc-unknown-none-elf/release/uart-demo.bin
         ///
         ///     Output: target/riscv64gc-unknown-none-elf/release/uart-demo.img.
         ///
         /// Specifying custom output path
         ///
-        ///     cargo xtask  gen-firmware -i target/riscv64gc-unknown-none-elf/release/uart-demo.bin -o ./uart-demo.x
+        ///     cargo xtask  gen-image -i target/riscv64gc-unknown-none-elf/release/uart-demo.bin -o ./uart-demo.x
         ///
         ///     Output: ./uart-demo.x
-        #[arg(long = "ofile", short = 'o')]
-        output: Option<String>,
+        #[arg(long = "output", short = 'o')]
+        output: Option<PathBuf>,
         /// Encryption type (optional).
         ///
         /// Parameter:
@@ -50,6 +55,6 @@ pub enum Command {
         ///
         ///     aes: AES-GCM + RSA-2048
         #[arg(long, short = 'e')]
-        encryption: Option<generate::firmware::EncryptionType>,
+        encryption: Option<EncryptionType>,
     },
 }
