bd881b1b18aecefcd7bfec2a08c8e393296ac08b
zhenglongbing <1211998648@qq.com>
2025-05-29 23:33:18 +0800

    feat(gpio): add Instance struct and SharedInstance impl
    Signed-off-by: Longbing Zheng <1211998648@qq.com>

--- kendryte-hal/src/gpio/mod.rs
+++ kendryte-hal/src/gpio/mod.rs
@@ -1,2 +1,12 @@
 mod register;
+use crate::instance::SharedInstance;
 pub use register::*;
+
+#[repr(transparent)]
+pub struct Instance<const N: usize> {
+    inner: RegisterBlock,
+}
+
+impl<const N: usize> SharedInstance for Instance<N> {
+    type Target = RegisterBlock;
+}
