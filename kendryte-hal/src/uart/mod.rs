bf1738691125e52b20651822058b5a7c021177d9
zhenglongbing <1211998648@qq.com>
2025-05-29 23:33:57 +0800

    feat(uart): add Instance struct and SharedInstance impl
    Signed-off-by: Longbing Zheng <1211998648@qq.com>

--- kendryte-hal/src/uart/mod.rs
+++ kendryte-hal/src/uart/mod.rs
@@ -1,2 +1,12 @@
 mod register;
+use crate::instance::ExclusiveInstance;
 pub use register::*;
+
+#[repr(transparent)]
+pub struct Instance<const N: usize> {
+    inner: RegisterBlock,
+}
+
+impl<const N: usize> ExclusiveInstance for Instance<N> {
+    type Target = RegisterBlock;
+}
