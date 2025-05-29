93c12670b6937728d85ef81d50567a2f14058f19
zhenglongbing <1211998648@qq.com>
2025-05-29 23:39:20 +0800

    feat(iomux): add Instance struct and SharedInstance impl
    Signed-off-by: Longbing Zheng <1211998648@qq.com>

--- kendryte-hal/src/iomux/mod.rs
+++ kendryte-hal/src/iomux/mod.rs
@@ -1,2 +1,12 @@
 mod register;
+use crate::instance::ExclusiveInstance;
 pub use register::*;
+
+#[repr(transparent)]
+pub struct Instance {
+    inner: RegisterBlock,
+}
+
+impl ExclusiveInstance for Instance {
+    type Target = RegisterBlock;
+}
